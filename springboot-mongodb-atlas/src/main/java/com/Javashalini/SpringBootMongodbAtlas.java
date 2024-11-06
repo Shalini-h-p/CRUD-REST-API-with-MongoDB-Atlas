95161bd2ad689f2f45c6bf6dd812c6d35c7d0c68
Shalini-h-p <shalinihp77@gmail.com>
2024-11-06 12:23:30 -0500

    first commit

---
+++ springboot-mongodb-atlas/src/main/java/com/Javashalini/SpringBootMongodbAtlas.java
@@ -0,0 +1,13 @@
+package com.Javashalini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbAtlas{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbAtlas.class, args);
+	}
+
+}
